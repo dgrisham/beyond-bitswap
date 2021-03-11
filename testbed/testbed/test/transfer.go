70de8cc1496f0559bc1e17ead3ed54c9f9ab84b7
dgrisham <dmgrisham@gmail.com>
2021-03-10 22:10:56 -0700

    uncomment ledger init code

--- testbed/testbed/test/transfer.go
+++ testbed/testbed/test/transfer.go
@@ -183,22 +183,22 @@ func Transfer(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
 				return err
 			}
 
-			// for _, peerInfo := range t.peerInfos {
-			// 	// runenv.RecordMessage("Looking for number of bytes to send for %s %d (peer %s)", peerInfo.Nodetp, peerInfo.TpIndex, peerInfo.Addr.ID.String())
+			for _, peerInfo := range t.peerInfos {
+				// runenv.RecordMessage("Looking for number of bytes to send for %s %d (peer %s)", peerInfo.Nodetp, peerInfo.TpIndex, peerInfo.Addr.ID.String())
 
-			// 	numBytesSent := getInitialSend(t.nodetp, t.tpindex, peerInfo.Nodetp, peerInfo.TpIndex)
-			// 	if numBytesSent != 0 {
-			// 		runenv.RecordMessage("Adding %d bytes to sent value in ledger for %s %d (peer %s)", numBytesSent, peerInfo.Nodetp, peerInfo.TpIndex, peerInfo.Addr.ID.String())
-			// 		bsnode.Bitswap.AddToLedgerSentBytes(peerInfo.Addr.ID, numBytesSent)
-			// 	}
+				numBytesSent := getInitialSend(t.nodetp, t.tpindex, peerInfo.Nodetp, peerInfo.TpIndex)
+				if numBytesSent != 0 {
+					runenv.RecordMessage("Adding %d bytes to sent value in ledger for %s %d (peer %s)", numBytesSent, peerInfo.Nodetp, peerInfo.TpIndex, peerInfo.Addr.ID.String())
+					bsnode.Bitswap.AddToLedgerSentBytes(peerInfo.Addr.ID, numBytesSent)
+				}
 
-			// 	// runenv.RecordMessage("Looking for number of bytes to recv from %s %d (peer %s)", peerInfo.Nodetp, peerInfo.TpIndex, peerInfo.Addr.ID.String())
-			// 	numBytesRcvd := getInitialSend(peerInfo.Nodetp, peerInfo.TpIndex, t.nodetp, t.tpindex)
-			// 	if numBytesRcvd != 0 {
-			// 		runenv.RecordMessage("Adding %d bytes to received value ledger for %s %d (peer %s)", numBytesRcvd, peerInfo.Nodetp, peerInfo.TpIndex, peerInfo.Addr.ID.String())
-			// 		bsnode.Bitswap.AddToLedgerReceivedBytes(peerInfo.Addr.ID, numBytesRcvd)
-			// 	}
-			// }
+				// runenv.RecordMessage("Looking for number of bytes to recv from %s %d (peer %s)", peerInfo.Nodetp, peerInfo.TpIndex, peerInfo.Addr.ID.String())
+				numBytesRcvd := getInitialSend(peerInfo.Nodetp, peerInfo.TpIndex, t.nodetp, t.tpindex)
+				if numBytesRcvd != 0 {
+					runenv.RecordMessage("Adding %d bytes to received value ledger for %s %d (peer %s)", numBytesRcvd, peerInfo.Nodetp, peerInfo.TpIndex, peerInfo.Addr.ID.String())
+					bsnode.Bitswap.AddToLedgerReceivedBytes(peerInfo.Addr.ID, numBytesRcvd)
+				}
+			}
 
 			// Wait for all nodes to initialize their ledgers
 			err = signalAndWaitForAll("ledger-initialization-complete-" + runID)
